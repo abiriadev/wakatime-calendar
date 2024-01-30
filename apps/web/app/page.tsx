bae73002030c963228dc32a5f0e9a0977d14c3ef
Abiria <abiria.dev@gmail.com>
2024-01-30 18:28:56 +0900

    feat: use fetch function to render calendar

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,6 +1,14 @@
 import ActivityCalendar from 'react-activity-calendar'
 
-export default function Page(): JSX.Element {
+import { fetchData } from '@repo/api'
+
+export default async function Page() {
+	const data = await fetchData(
+		process.env.WAKATIME!,
+		new Date('2023-01-01'),
+		new Date('2023-12-31'),
+	)
+
 	return (
 		<main>
 			<ActivityCalendar data={[]} />
