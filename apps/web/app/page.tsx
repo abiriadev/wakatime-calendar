e279d6ed995b5c873df93d3bbb292ad068390e94
Abiria <abiria.dev@gmail.com>
2024-02-01 11:05:05 +0900

    feat: fetch wakatime-colors and use interner

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,6 +1,7 @@
 import ActivityCalendar from 'react-activity-calendar'
 
 import { fetchData } from '@repo/api'
+import { Interner } from './interner'
 
 export default async function Page() {
 	console.log(process.env.WAKATIME)
@@ -11,14 +12,33 @@ export default async function Page() {
 		new Date('2023-12-31'),
 	)
 
+	const interner = new Interner()
+
+	interner.bulkInsert(
+		(await (
+			await fetch(
+				'https://github.com/abiriadev/wakatime-colors/raw/main/colors.json',
+			)
+		).json()) as Record<string, string>,
+	)
+
+	const palette = interner.dumpValues()
+
 	return (
 		<main>
 			<ActivityCalendar
 				data={data.map(({ date, lang }) => ({
 					date,
-					level: (lang?.length ?? 0) % 5,
+					level:
+						lang === null
+							? 0
+							: interner.lookup(lang) ?? 0,
 					count: 1,
 				}))}
+				theme={{
+					light: palette,
+					dark: palette,
+				}}
 			/>
 		</main>
 	)
